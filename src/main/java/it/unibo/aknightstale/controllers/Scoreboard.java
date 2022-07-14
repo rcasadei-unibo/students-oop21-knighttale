e34ccb7b0fe4c50ab29d5dea1452e48885dc9568
Maicol Battistini <maicolbattistini@live.it>
2022-07-14 20:37:50 +0200

    fix: Fix crash when scoreboard is loaded before the main menu

--- src/main/java/it/unibo/aknightstale/controllers/Scoreboard.java
+++ src/main/java/it/unibo/aknightstale/controllers/Scoreboard.java
@@ -3,17 +3,19 @@ package it.unibo.aknightstale.controllers;
 import it.unibo.aknightstale.controllers.interfaces.MainMenuController;
 import it.unibo.aknightstale.controllers.interfaces.ScoreboardController;
 import it.unibo.aknightstale.controllers.utils.ControllerFactory;
+import it.unibo.aknightstale.views.interfaces.MainMenuView;
 import it.unibo.aknightstale.views.interfaces.ScoreboardView;
 
 import java.util.Map;
 import java.util.Set;
 
 public class Scoreboard extends BaseController<ScoreboardView> implements ScoreboardController {
-    private final it.unibo.aknightstale.models.Scoreboard scoreboard = new it.unibo.aknightstale.models.Scoreboard();
+    private final it.unibo.aknightstale.models.Scoreboard scoreboard = new it.unibo.aknightstale.models.Scoreboard(); //NOPMD - suppressed AvoidFieldNameMatchingTypeName - To remove when renaming MVC
 
     /**
      * Get scoreboard values.
      */
+    @Override
     public Set<Map.Entry<String, Integer>> getScoreboard() {
         return this.scoreboard.getEntries();
     }
@@ -21,8 +23,9 @@ public class Scoreboard extends BaseController<ScoreboardView> implements Scoreb
     /**
      * Return to main menu.
      */
+    @Override
     public void returnToMainMenu() {
-        ControllerFactory.createController(MainMenuController.class).showView();
+        ControllerFactory.createController(MainMenuController.class, MainMenuView.class).showView();
     }
 
     /**
