bb257135e23f50a064e738f8a6c3c10cd8787e64
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-08-01 10:09:00 +0200

    added entitymanager interface

--- src/main/java/it/unibo/aknightstale/utility/EntityManager.java
+++ src/main/java/it/unibo/aknightstale/utility/EntityManager.java
@@ -1,14 +1,31 @@
 package it.unibo.aknightstale.utility;
 
+import java.util.List;
 
-import it.unibo.aknightstale.entity.EntityController;
+import it.unibo.aknightstale.entity.controller.EntityController;
 
 public interface EntityManager {
+	
     void update();
 
+    /**
+     * Add a new entity in the entity list if it is not already present
+     * 
+     * @param ec	The new entity that will be added if it is not already present
+     */
     void addEntity(EntityController ec);
+    
+    /**
+     * Remove a entity from the entity list if it is present
+     * 
+     * @param ec	The entity will be removed if it is present
+     */
     void removeEntity(EntityController ec);
 
-    //non è deciso se array o mappa o lista o set ecc...
-    EntityController[] getEntities();
+    /**
+     * Get the entity list
+     * 
+     * @return The entity list
+     */
+    List<EntityController> getEntities();
 }
