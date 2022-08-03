83df5e62c00ff99db40238ba1f1a6c71c5d69cf4
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-08-03 08:51:25 +0200

    completed player controller

--- src/main/java/it/unibo/aknightstale/entity/controller/PlayerController.java
+++ src/main/java/it/unibo/aknightstale/entity/controller/PlayerController.java
@@ -7,9 +7,12 @@ import it.unibo.aknightstale.entity.view.AnimatedEntityView;
 import it.unibo.aknightstale.utility.EntityManager;
 
 public class PlayerController<M extends Character, V extends AnimatedEntityView> extends AbstractController<M, V> {
+	
+	private final EntityManager manager;
 
 	public PlayerController(M model, V view, EntityManager manager) {
-		super(model, view, manager);
+		super(model, view);
+		this.manager = manager;
 	}
 
 	@Override
@@ -21,7 +24,33 @@ public class PlayerController<M extends Character, V extends AnimatedEntityView>
 
 	@Override
 	public void attack() {
-		
+		this.manager.getEntities()
+		.stream()
+		.filter(ec -> {
+			var e = ec.getModel();
+			switch (this.model.getDirection()) {
+			case RIGHT:
+				return e.getPosition().getX() <= this.model.getPosition().getX() + this.model.getAttackRange() &&
+						e.getPosition().getX() >= this.model.getPosition().getX();
+			case LEFT:
+				return e.getPosition().getX() >= this.model.getPosition().getX() - this.model.getAttackRange() &&
+						e.getPosition().getX() <= this.model.getPosition().getX();
+			case UP:
+				return e.getPosition().getY() >= this.model.getPosition().getY() - this.model.getAttackRange() &&
+						e.getPosition().getY() <= this.model.getPosition().getY();
+			case DOWN:
+				return e.getPosition().getY() <= this.model.getPosition().getY() + this.model.getAttackRange() &&
+						e.getPosition().getY() >= this.model.getPosition().getY();
+			}
+			return true;
+		})
+		.filter(ec -> ec.getModel() instanceof Character)
+		.forEach(e -> {
+			var model = (Character) e.getModel();
+			this.model.attack(model);
+		});
+	this.view.setStatus(Status.ATTACK);
+	this.view.update(this.model.getDirection());		
 	}
 
 }
