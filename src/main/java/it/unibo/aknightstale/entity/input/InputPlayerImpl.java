df5ce9ae06a06b11108bf8aad39cf6a577dcd555
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-08-03 10:59:46 +0200

    fixed input player

--- src/main/java/it/unibo/aknightstale/entity/input/InputPlayerImpl.java
+++ src/main/java/it/unibo/aknightstale/entity/input/InputPlayerImpl.java
@@ -3,15 +3,17 @@ package it.unibo.aknightstale.entity.input;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.unibo.aknightstale.entity.controller.EntityController;
+import it.unibo.aknightstale.entity.controller.CharacterController;
+import it.unibo.aknightstale.entity.model.CharacterModel;
+import it.unibo.aknightstale.entity.view.AnimatedEntityView;
 import javafx.scene.layout.Pane;
 
 public class InputPlayerImpl implements InputPlayer {
 	
-	private final EntityController player;
+	private final CharacterController<CharacterModel, AnimatedEntityView> player;
 	private List<String> keyPressed;
 
-	public InputPlayerImpl(EntityController player, Pane scene) {
+	public InputPlayerImpl(CharacterController<CharacterModel, AnimatedEntityView> player, Pane scene) {
 		super();
 		this.player = player;
 		this.keyPressed = new ArrayList<>();
@@ -32,13 +34,23 @@ public class InputPlayerImpl implements InputPlayer {
 	}
 
 	@Override
-	public void update() {/*
-							 * if (keyPressed.contains("A")) { this.player.moveLeft(); } if
-							 * (keyPressed.contains("D")) { this.player.moveRight(); } if
-							 * (keyPressed.contains("W")) { this.player.moveUp(); } if
-							 * (keyPressed.contains("S")) { this.player.moveDown(); } if
-							 * (keyPressed.contains("SPACE")) { this.player.attack(); }
-							 */
+	public void update() {
+		if (keyPressed.contains("A")) {
+			this.player.moveLeft();
+		}
+		if	(keyPressed.contains("D")) {
+			this.player.moveRight();
+		}
+		if		(keyPressed.contains("W")) {
+			this.player.moveUp(); 
+		}
+		if		(keyPressed.contains("S")) { 
+			this.player.moveDown(); 
+		}
+		if (keyPressed.contains("SPACE")) {
+			this.player.attack(); 
+		}
+							 
 	}
 
 }
