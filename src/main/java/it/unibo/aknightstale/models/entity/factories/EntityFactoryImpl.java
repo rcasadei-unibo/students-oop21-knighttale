4e9f2aa13fca91c4313366e05ec6a7251f4a2403
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-06 21:55:57 +0200

    add method getEnemy to EntityFactoryImpl class

--- src/main/java/it/unibo/aknightstale/models/entity/factories/EntityFactoryImpl.java
+++ src/main/java/it/unibo/aknightstale/models/entity/factories/EntityFactoryImpl.java
@@ -2,13 +2,16 @@ package it.unibo.aknightstale.models.entity.factories;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.aknightstale.controllers.entity.CharacterController;
+import it.unibo.aknightstale.controllers.entity.EnemyController;
 import it.unibo.aknightstale.controllers.entity.PlayerController;
+import it.unibo.aknightstale.models.Enemy;
 import it.unibo.aknightstale.models.entity.Character;
 import it.unibo.aknightstale.models.entity.Player;
 import it.unibo.aknightstale.utils.EntityManager;
 import it.unibo.aknightstale.utils.EntityManagerImpl;
 import it.unibo.aknightstale.utils.Point2D;
 import it.unibo.aknightstale.views.entity.AnimatedEntityView;
+import it.unibo.aknightstale.views.entity.EnemyView;
 import it.unibo.aknightstale.views.entity.PlayerView;
 
 public class EntityFactoryImpl implements EntityFactory {
@@ -41,5 +44,20 @@ public class EntityFactoryImpl implements EntityFactory {
         this.manager.addEntity(player);
         return player;
     }
+
+    @Override
+    public CharacterController<Character, AnimatedEntityView> getEnemy(final int x, final int y) {
+
+        Enemy enemyModel = new Enemy(new Point2D(x, y));
+        EnemyView enemyView = new EnemyView();
+        new EnemyController(enemyModel, enemyView, new EntityManagerImpl());
+
+        final var enemy = new PlayerController<Character, AnimatedEntityView>(new Player(SPAWN_PLAYER),
+                new PlayerView(), getEntityManager());
+        this.manager.addEntity(enemy);
+        return enemy;
+    }
+
+
 
 }
