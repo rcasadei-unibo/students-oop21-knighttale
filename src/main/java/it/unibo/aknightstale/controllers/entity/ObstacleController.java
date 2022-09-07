0225f533c9b1da66fb3843c97d2792bc876d96c1
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-07 21:35:59 +0200

    changed generics of ObstacleController class

--- src/main/java/it/unibo/aknightstale/controllers/entity/ObstacleController.java
+++ src/main/java/it/unibo/aknightstale/controllers/entity/ObstacleController.java
@@ -1,11 +1,11 @@
 package it.unibo.aknightstale.controllers.entity;
 
-import it.unibo.aknightstale.models.entity.ObstacleEntity;
-import it.unibo.aknightstale.views.map.SolidTile;
+import it.unibo.aknightstale.models.entity.Character;
+import it.unibo.aknightstale.views.entity.AnimatedEntityView;
 
-public class ObstacleController extends EntityControllerImpl <ObstacleEntity, SolidTile> {
+public class ObstacleController<M extends Character, V extends AnimatedEntityView> extends EntityControllerImpl<M, V> {
 
-    public ObstacleController(ObstacleEntity model, SolidTile view) {
+    public ObstacleController(final M model, final V view) {
         super(model, view);
     }
 }
