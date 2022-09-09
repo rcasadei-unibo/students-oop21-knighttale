2e58c363523c96d4079c5f0c7190c3402d1a4a74
Maicol Battistini <maicolbattistini@live.it>
2022-09-09 18:57:00 +0200

    test: Removed viewtest from models tests

--- src/test/java/it/unibo/aknightstale/models/entity/PlayerTest.java
+++ src/test/java/it/unibo/aknightstale/models/entity/PlayerTest.java
@@ -1,30 +1,15 @@
 package it.unibo.aknightstale.models.entity;
 
-import it.unibo.aknightstale.controllers.interfaces.MainMenuController;
 import it.unibo.aknightstale.models.entity.factories.EntityFactory;
 import it.unibo.aknightstale.models.entity.factories.EntityFactoryImpl;
 import it.unibo.aknightstale.utils.Point2D;
-import it.unibo.aknightstale.views.BaseViewTest;
-import it.unibo.aknightstale.views.interfaces.MainMenuView;
-import javafx.stage.Stage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.Start;
 
-// TODO: Replace with World view
-class PlayerTest extends BaseViewTest<MainMenuController, MainMenuView> {
+class PlayerTest {
+    private static final int ATTACK_TIMES = 40;
     private final EntityFactory factory = new EntityFactoryImpl();
-
-    PlayerTest() {
-        super(MainMenuView.class, MainMenuController.class);
-    }
-
-    @Start
-    @Override
-    public void start(final Stage stage) {
-        super.start(stage);
-    }
 
     @Test
     @DisplayName("Check type")
@@ -49,11 +34,10 @@ class PlayerTest extends BaseViewTest<MainMenuController, MainMenuView> {
         final var player = this.factory.getPlayer().getModel();
         final LifeEntity entity = this.factory.getEnemy(player.getPosition().getX(), player.getPosition().getY())
                 .getModel();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < ATTACK_TIMES; i++) {
             player.attack(entity);
         }
         Assertions.assertThat(entity.isDead()).isTrue();
-        // TODO: test defense when calculating damage
     }
 
     @Test
@@ -64,7 +48,7 @@ class PlayerTest extends BaseViewTest<MainMenuController, MainMenuView> {
                 .getModel();
         final var life = entity.getHealth();
         player.attack(entity);
-        Assertions.assertThat(entity.getHealth()).isEqualTo(life - player.getDamage());
+        Assertions.assertThat(entity.getHealth()).isEqualTo(life - (player.getDamage() * (entity.getDefense() / 100)));
     }
 
     @Test
