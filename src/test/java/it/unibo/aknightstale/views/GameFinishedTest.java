ab20154e464516b3ecaa6b391dafddebd7d5d2e9
Maicol Battistini <maicolbattistini@live.it>
2022-09-09 19:59:06 +0200

    test: Added new score test

--- src/test/java/it/unibo/aknightstale/views/GameFinishedTest.java
+++ src/test/java/it/unibo/aknightstale/views/GameFinishedTest.java
@@ -1,6 +1,7 @@
 package it.unibo.aknightstale.views;
 
 import it.unibo.aknightstale.controllers.interfaces.GameFinishedController;
+import it.unibo.aknightstale.models.ScoreboardImpl;
 import it.unibo.aknightstale.views.interfaces.GameFinishedView;
 import it.unibo.aknightstale.views.interfaces.MainMenuView;
 import it.unibo.aknightstale.views.interfaces.MapView;
@@ -48,6 +49,19 @@ class GameFinishedTest extends BaseViewTest<GameFinishedController, GameFinished
     void checkScoreLabel(final FxRobot robot) {
         final var score = Integer.parseInt(robot.lookup("#scoreLabel").queryAs(Label.class).getText());
         Assertions.assertThat(score).isEqualTo(SCORE).isEqualTo(getController().getScore());
+    }
+
+    @Test
+    @DisplayName("Checks the scoreboard after saving the score")
+    void checkNewScoreAfterSaving(final FxRobot robot) {
+        final var score = Integer.parseInt(robot.lookup("#scoreLabel").queryAs(Label.class).getText());
+        robot.clickOn("#nameTextField");
+        robot.write("TestFXPlayer");
+        robot.clickOn("#saveScoreButton");
+
+        final var scoreboard = new ScoreboardImpl();
+        scoreboard.load();
+        Assertions.assertThat(scoreboard.getScore("TestFXPlayer")).isEqualTo(score);
     }
 
 
